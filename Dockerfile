03e848175436bae90e606cb5eb2cdd8f26d8147c
bbursey <bbursey3@gatech.edu>
2021-11-01 23:56:12 -0400

    move files to /src folder. Add dockerfile and deployment.yaml

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+from python:3.6
+
+RUN mkdir /app
+WORKDIR /app
+ADD . /app/
+RUN pip install -r requirements.txt
+
+EXPOSE 5000
+CMD ["python", "/app/src/app.py"]
---
+++ src/app.py
@@ -0,0 +1,38 @@
+from lp import LP
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Greetings"
+
+
+@app.route("/budget")
+def budget():
+    budget = {
+        "income": (5000.00, 5000.00),
+        "rent": (600.00, 600.00),
+        "loans": (300.00, 700.00),
+        "invest": (400.00, 600.00),
+        "savings": (750.00, 1200.00),
+        "groceries": (100.00, 400.00),
+        "car": (200.00, 300.00),
+        "tythe": (500.00, 500.00),
+        "clothes": (50.00, 100.00),
+        "gym": (30.00, 30.00),
+        "emergency": (200.00, 400.00),
+        "fun": (200.00, 300.00)
+    }
+    lp = LP(budget)
+    results = lp.solve()
+    lp.get_stats(budget)
+    return ""
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
+
+# TODO: setup config for this to be deployed
+# TODO: add code for discrete values (ex: subscriptions)
+# TODO: Look into Quadratic Programming for more complex items like loans (quadprog)
